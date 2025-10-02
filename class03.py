25e65c1e2a3efd2ccb67c059526a46f2459e900f
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-03 01:11:48 +0600

    problem solving

---
+++ class03.py
@@ -0,0 +1,17 @@
+#Problem solving from class 1,2
+
+#input 2 number and print their sum
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("The sum is:", a+b)
+
+#calculate area of a rectangle
+side=float(input("Enter first number: "))
+width=float(input("Enter second number: "))
+print("The area of a rectangle is:", a*b)
+
+#Take 2 floating number input and print their avg
+a=float(input("Enter any number: "))
+b=float(input("Enter any number: "))
+avg=(a+b)/2
+print("The avg is: ", avg)
