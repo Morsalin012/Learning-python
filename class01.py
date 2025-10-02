5e65d7ba170f0dec21af8b71908385ae78c02f24
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-02 11:57:54 +0600

    Initialize project structure and add initial Python files

---
+++ class01.py
@@ -0,0 +1,15 @@
+#Variable and data types
+
+name= "Morsalin"
+age= 25
+price= 45.00
+a= None
+old= False
+new= True
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(a))
+print(type(old))
+print(type(new))
