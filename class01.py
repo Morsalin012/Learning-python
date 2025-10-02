05669bb4d79442655c2c0aea1d29b84103be5e17
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-03 00:38:14 +0600

    Add type conversion examples to demonstrate variable types and operations

--- class01.py
+++ class01.py
@@ -13,3 +13,14 @@ print(type(price))
 print(type(a))
 print(type(old))
 print(type(new))
+
+#type conversion
+x= 5
+y= 2.5
+z= int("10")
+print(type(z))
+print(x+y+z)
+
+a=5
+a= float(a)
+print(type(a))
