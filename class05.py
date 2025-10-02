ebf1016da09c6c4049b619be75741ca6c6b0d72c
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-03 03:44:56 +0600

    some string fucntions

---
+++ class05.py
@@ -0,0 +1,7 @@
+#String Functions
+str="i am a student."
+print(str.endswith("nt.")) #returns true if string ends with substr
+print(str.capitalize()) #first letter capital
+print(str.replace("student", "teacher")) #changes strings
+print(str.find("a")) #returns 1st index of 1st occurence
+print(str.count("am")) #count the string
