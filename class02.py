5d3d1d830a65517e2ebc9295236b87d4f088449e
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-03 01:11:25 +0600

    Add user input handling and type demonstration in class02.py

---
+++ class02.py
@@ -0,0 +1,19 @@
+#input from user
+
+name= input("Enter your name: ")
+print("Hello", name)
+
+x=input("Enter any value: ")
+print(type(x), x)
+
+y=int(input("Enter any value: "))
+print(type(y), y)
+
+y=float(input("Enter any value: "))
+print(type(y), y)
+
+name= input("Enter your name: ")
+age= int(input("Enter your age: "))
+
+print("Welcome ", name, "and your age is", age)
+
