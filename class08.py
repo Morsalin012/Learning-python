563584995772fb26a4e407155c87f6a6ce630e64
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-20 11:43:18 +0600

    Add class08.py with variable type annotations

---
+++ class08.py
@@ -0,0 +1,11 @@
+#Variables annotated by type
+
+import typing
+a: str = "Hello, World!"
+b: int = 42
+c: float = 3.14
+
+list_of_numbers: typing.List[int] = [1, 2, 3]
+tuple_of_numbers: typing.Tuple[int, int, int] = (1, 2, 3)
+dictionary: typing.Dict[str, int] = {"key1": 1, "key2": 2}
+set_of_numbers: typing.Set[int] = {1, 2, 3}
