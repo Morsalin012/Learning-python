90110ed00c50e0d5d25a1ec55c091e7ea775216c
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-20 13:27:56 +0600

    Add class10.py with username validation using if-else statements

---
+++ class10.py
@@ -0,0 +1,9 @@
+#if-else statements
+def hint_username(username):
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
+    else:    
+       print("Valid username")
+hint_username("Morsalin")
+hint_username("Al")
+
