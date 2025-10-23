97f6e6c0ba38c21de0bed9f87c5379b6b725acbc
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-24 01:01:23 +0600

    examples of while loops

--- class12.py
+++ class12.py
@@ -31,3 +31,34 @@ while x<10:
     x=x+1
 print("Total product = ", product)
  
+
+# example 4
+def count_factors(given_number):
+    factor=1
+    count=1
+    if given_number==0:
+        return 0
+    while factor<given_number:
+        if given_number%factor==0:
+            count+=1
+        factor+=1
+    return count
+print(count_factors(0))
+print(count_factors(2))
+print(count_factors(4))
+print(count_factors(6))
+print(count_factors(10))
+
+#Example 5
+def addition_table(given_number):
+    interated_number=1
+    my_sum=1
+    while interated_number<=5:
+        my_sum= given_number+interated_number
+        if my_sum>20:
+            break
+        print(str(given_number), "+", str(interated_number), "=", str(my_sum))
+        interated_number+=1
+
+addition_table(4)
+addition_table(5)
