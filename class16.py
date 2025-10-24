c6219ed11e9f145b28298eeab5c2fec440a54026
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-24 19:15:15 +0600

    Add recursion examples for factorial, power check, and sum of positive numbers

---
+++ class16.py
@@ -0,0 +1,33 @@
+#Recursion in Python
+
+# Factorial of a number using recursion
+def factorial(n):
+    if n<2:
+        return 1
+    return n*factorial(n-1) 
+print(factorial(999))
+
+
+# Check if a number is a power of a given base using recursion
+def is_power_of(number, base):
+ # Base case: when number is smaller than base.
+    if number < base:
+        # If number is equal to 1, it's a power (base**0).
+        return number==1
+
+    # Recursive case: keep dividing number by base.
+    return is_power_of(number/base, base)
+
+print(is_power_of(8,2))     # Should be True
+print(is_power_of(64,4))    # Should be True
+print(is_power_of(70,10))   # Should be False
+
+
+# Sum of positive numbers up to n using recursion
+def sum_positive_numbers(n):
+    if n < 1:
+        return 0
+    return n+sum_positive_numbers(n-1)
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
