ceb2cb97576ac64801a85d4d07413beb7d0b67d1
Muhammad Morsalin <muhammadmorsalin12@gmail.com>
2025-10-24 15:26:42 +0600

    Examples of slicing and joining strings

--- class15.py
+++ class15.py
@@ -1,17 +1,17 @@
-# #slicing and joining strings
+#slicing and joining strings
 
-# #slicing strings
-# string1= "Greetings, Earthlings"
-# print(string1[0])#first character
-# print(string1[2:5])
-# print(string1[:5])#from the start to index 5
-# print(string1[11:])#count from the forwards
-# print(string1[-10:])#count from the backwards
-# print(string1[::2])#every second character
-# print(string1[55:])#out of range
-# print(string1[:])#the whole string
-# print(string1[::-1])#the whole string reversed
-# print(string1[::-2])#count every second character and reversed the string
+#slicing strings
+string1= "Greetings, Earthlings"
+print(string1[0])#first character
+print(string1[2:5])
+print(string1[:5])#from the start to index 5
+print(string1[11:])#count from the forwards
+print(string1[-10:])#count from the backwards
+print(string1[::2])#every second character
+print(string1[55:])#out of range
+print(string1[:])#the whole string
+print(string1[::-1])#the whole string reversed
+print(string1[::-2])#count every second character and reversed the string
 
 #joining strings
 string2=["Hello","from","the","other","side"]
